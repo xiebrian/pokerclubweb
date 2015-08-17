525df26570abae75986e82babbb228bc766f9835
Justin Martinez <jlmart88@mit.edu>
2015-08-17 07:50:21 -0700

    static

--- pokerclubweb/wsgi/production.py
+++ pokerclubweb/wsgi/production.py
@@ -1,10 +1,11 @@
 from django.core.wsgi import get_wsgi_application
 from . import bootstrap_wsgi_settings
-from whitenoise.django import DjangoWhiteNoise
 
 # Add the project to sys.path,
 # set DJANGO_SETTINGS_MODULE to PROJECT_NAME.settings.env.THIS_FILENAME
 bootstrap_wsgi_settings(__file__)
+
+from whitenoise.django import DjangoWhiteNoise
 
 # Export application object
 application = get_wsgi_application()
